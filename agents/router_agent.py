b4bb6cc999f89442b0df4f79072d2cde0aa4d655
Kaveesha358 <ksdes358@gmail.com>
2026-07-29 11:28:54 +0000

    Fix: Remove google.colab dependency from router_agent

--- agents/router_agent.py
+++ agents/router_agent.py
@@ -1,10 +1,9 @@
 import os
-from google.colab import userdata
-
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage
 
-os.environ["GROQ_API_KEY"] = userdata.get('GROQ_API_KEY')
+if "GROQ_API_KEY" not in os.environ:
+    os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY", "")
 
 llm = ChatGroq(
     model="llama-3.1-8b-instant",
